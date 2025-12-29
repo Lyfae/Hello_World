ac62ee94f202065fa0ed95e75569814d27211d69
Paul <usuikagedestiny@gmail.com>
2025-12-29 02:20:48 +0000

    Added more information to the cout

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,5 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World! This is Paul! \n"; }
+int main() {
+  std::cout << "Hello World! This is Paul! I am a person who likes to code. \n";
+}
